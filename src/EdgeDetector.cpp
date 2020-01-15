3683e9a2bf2859c7e6fccf1f41eac84835e5af3d
r-AZAD-jr <55370357+r-AZAD-jr@users.noreply.github.com>
2020-01-15 16:26:29 +0100

    Update EdgeDetector.cpp

--- src/EdgeDetector.cpp
+++ src/EdgeDetector.cpp
@@ -9,16 +9,16 @@ int main()
 	 We are calling getInputImage function in EdgeDetector class.
 	 getInputImage takes the image path as input and returns back the image
 	 as cv::Mat format.
-	 */
+	*/
 
 	// We can simply change the name of the image and it will give us the corresponding output.
 	cv::Mat originalInputImage = detector.getInputImage("data/Image_4.png");
 
 	/*
-     	We are calling the functions of EdgeDetector to get the final image
-     	superimposed with edge detected in green color on the original image.
-     	We will find more detail about the functions inside the EdgeDetector class definition.
-	 */
+     	 We are calling the functions of EdgeDetector to get the final image
+     	 superimposed with edge detected in green color on the original image.
+     	 We will find more detail about the functions inside the EdgeDetector class definition.
+	*/
 
 	detector.showImage(
 			detector.colorChangeAndSuperimposed(
